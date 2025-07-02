b7d90e7221716fad6103c198f0aedb1abdcd0e0e
farunurisonmez <info@farunurisonmez.com>
2025-07-03 00:10:38 +0300

    feat(graphql): add basic Query type and map 'instructions' field to GraphQL endpoint

--- graphql-dotnet-starter/Extensions/ServiceCollectionExtensions.cs
+++ graphql-dotnet-starter/Extensions/ServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@
     {
         public static void ConfigureServices(this WebApplicationBuilder builder)
         {
-            builder.Services.AddGraphQLServer();
+            builder.Services.AddGraphQLServer().AddQueryType<Query>();
         }
     }
 }
---
+++ graphql-dotnet-starter/Query.cs
@@ -0,0 +1,7 @@
+﻿namespace graphql_dotnet_starter
+{
+    public class Query
+    {
+        public string Instructions => "Hello GraphQL!";
+    }
+}
